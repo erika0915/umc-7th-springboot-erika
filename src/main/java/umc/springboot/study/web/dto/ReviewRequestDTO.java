f4742afe2194f4bb533844a63d430b4f6d8396ea
erika0915 <soohee2001@gmail.com>
2024-12-18 01:01:23 +0900

    chore: 가게의 리뷰 추가하기 API 구현에 DTO수정

--- src/main/java/umc/springboot/study/web/dto/ReviewRequestDTO.java
+++ src/main/java/umc/springboot/study/web/dto/ReviewRequestDTO.java
@@ -1,14 +1,23 @@
 package umc.springboot.study.web.dto;
 
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 
 public class ReviewRequestDTO {
     // 리뷰 추가 요청 DTO
     @Getter
     public static class AddReviewDTO{
+        @NotNull(message = "회원 ID는 필수 입력 항목입니다.")
         private Long userId;
+        @NotNull(message = "가게 ID는 필수 입력 항목입니다.")
         private Long storeId;
+        @NotBlank(message = "리뷰 내용은 필수 입력 항목입니다.")
         private String content;
+        @Min(value = 1, message = "평점은 최소 1점입니다.")
+        @Max(value = 5, message = "평점은 최대 5점입니다.")
         private Float score;
     }
 }
